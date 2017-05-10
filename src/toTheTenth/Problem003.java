ff8434e4afa792415a077fe97eaa5af215d961ac
bit-user <bit-user@bit>
2017-05-10 20:16:55 +0900

    zz

--- src/toTheTenth/Problem003.java
+++ src/toTheTenth/Problem003.java
@@ -4,19 +4,19 @@ package toTheTenth;
 public class Problem003 {
 
 	public static void main(String[] args) {
-		int n = 13195;
-		int divisor = 0;
-		int res = 0;
-
-		
-		for (int i = 1; i < n; i++) {
-			
-			if (n % i == 0 ) { //약수
-				//소수 구하는 공식
-				
+		//long begin = System.currentTimeMillis();
+		long n=600851475143L;
+		for (long i = 2; i <= n; i++)
+		{
+			if (n % i==0)
+			{
 				System.out.println(i);
+				n = n / i;
+				i = 2;
 			}
 		}
-	}
+		//long end = System.currentTimeMillis();
+		//System.out.println(end-begin + "ms");
 
+	}
 }
